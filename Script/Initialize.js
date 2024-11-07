7a1aba464f8d036734efe30f576d79c334f469a8
annnou <87457668+annnou@users.noreply.github.com>
2024-11-07 22:28:13 +0900

    [fix]こまごま

--- Script/Initialize.js
+++ Script/Initialize.js
@@ -5,14 +5,16 @@
 
 // Three.jsシーンの初期化
 export function initializeScene(){
+    //シーンの追加
     const scene = new THREE.Scene();
+    //カメラの追加
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
     camera.position.set(0, 1.7, -10);
     camera.lookAt(scene.position);
+    //描画の設定
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
     return {scene,camera,renderer};
 }
 
--- Script/MainScript.js
+++ Script/MainScript.js
@@ -8,11 +8,15 @@ import { initializeScene } from './Initialize.js';
 import { globalValiable } from '../Data/data.js';
 
 //初期化処理
-const {scene, camera, renderer } = initializeScene();
-
+const {scene, camera, renderer} = initializeScene();
+//ローダーの追加
+const loader = new THREE.GLTFLoader();
 // Raycasterとマウス位置の初期化
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
+
+//Main Class Valiable
+let loadedObject;
 
 // 照明の追加
 const Firstlight = new THREE.DirectionalLight(0xffffff, 1);
@@ -28,57 +32,58 @@ const groundMaterial = new THREE.MeshStandardMaterial({ color: new THREE.Color(0
 const ground = new THREE.Mesh(groundGeometry, groundMaterial);
 ground.rotation.x = - Math.PI / 2;
 ground.position.y = 0; 
-
+// シーンに地平線（地面）を追加
+scene.add(ground);
+//背景色追加
+scene.background = new THREE.Color(0.5,1,1);
 //アニメーションミキサーの定義
 let mixer;
 
-// シーンに地平線（地面）を追加
-scene.add(ground);
+function main(){
+    LoadModel();
+    Animate();
+    MoveCamera();
+}
 
-//背景色追加
-scene.background = new THREE.Color(0.5,1,1);
+//モデルのロード関数
+function LoadModel(){
+    loader.load(
+        'https://warashisan.github.io/Resource/3dObject/ClickButton.glb',
+        function (gltf) {
+            loadedObject = gltf.scene;
 
-// GLTFモデルのロード
-const loader = new THREE.GLTFLoader();
-let loadedObject;
-loader.load(
-    'https://warashisan.github.io/Resource/3dObject/ClickButton.glb',
-    function (gltf) {
-        loadedObject = gltf.scene;
-        
-        loadedObject.traverse((child) =>{
-            if(child.isMesh){
-                child.material.color.set(0xffffff);
-            }
-        });
+            loadedObject.traverse((child) =>{
+                if(child.isMesh){
+                    child.material.color.set(0xffffff);
+                }
+            });
 
-        scene.add(loadedObject);
-    },
-    undefined,
-    function (error) {
-        console.error('Error loading model:', error);
-    }
-);
-loader.load('https://warashisan.github.io/Resource/gif/Ghost_1,0.glb', 
-    function (gltf) {
-        gltf.scene.position.z = 3;
-        scene.add(gltf.scene);
-
-        //アニメーションが含まれている場合、再生する
-        if (gltf.animations.length > 0) {
-            mixer = new THREE.AnimationMixer(gltf.scene);
-                gltf.animations.forEach((clip) => {
-                    mixer.clipAction(clip).play();
-                });
+            scene.add(loadedObject);
+        },
+        undefined,
+        function (error) {
+            console.error('Error loading model:', error);
         }
-        animate();
-        moveCamera();
-    }, 
-    undefined, 
-    function (error) {
-        console.error(error);
-    }
-);
+    );
+    loader.load('https://warashisan.github.io/Resource/gif/Ghost_1,0.glb', 
+        function (gltf) {
+            gltf.scene.position.z = 3;
+            scene.add(gltf.scene);
+
+            //アニメーションが含まれている場合、再生する
+            if (gltf.animations.length > 0) {
+                mixer = new THREE.AnimationMixer(gltf.scene);
+                    gltf.animations.forEach((clip) => {
+                        mixer.clipAction(clip).play();
+                    });
+            }
+        }, 
+        undefined, 
+        function (error) {
+            console.error(error);
+        }
+    );
+}
 
 // マウスムーブイベントをリスナーに追加
 window.addEventListener('mousemove', onMouseMove, false);
@@ -125,9 +130,9 @@ function onClick(event) {
     }
 }
 
-// アニメーション関数
-function animate() {
-    requestAnimationFrame(animate);
+// アニメーション関数（ゴース）
+function Animate() {
+    requestAnimationFrame(Animate);
     //アニメーションが含まれていれば更新
     if (mixer) {
         mixer.update(0.006);//適切な更新速度を設定
@@ -136,14 +141,16 @@ function animate() {
 }
 
 let count = 0;
-function moveCamera(){
-    if(count >= 10000)return;
+//カメラを少しずつ動かす
+function MoveCamera(){
+    if(count >= 1000)return;
     camera.position.y += 0.006;
     count++;
     renderer.render(scene,camera);
-    requestAnimationFrame(moveCamera);
+    requestAnimationFrame(MoveCamera);
 }
 
+//ログの追加
 function displayLog(message) {
     const logDiv = document.getElementById('log');
     const newLog = document.createElement('p'); // 新しいログ行を追加
@@ -153,3 +160,5 @@ function displayLog(message) {
     // ログエリアがいっぱいになったら自動的にスクロール
     logDiv.scrollTop = logDiv.scrollHeight;
 }
+
+window.onload = main;
